3eb20f164e68dcccc995c454a2492f5fd18f98e3
Chanatip Kriengkraipetch <chanatip.kr@kbtg.tech>
2018-06-27 16:35:16 +0700

    Add NotEnoughBooksInStock Exception test

---
+++ src/test/java/testHarryPotter/TestNotEnoughBooksInStock.java
@@ -0,0 +1,27 @@
+package testHarryPotter;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import exceptionDemo.IllegalDivisionException;
+import harryPotter.Book;
+import harryPotter.BookItem;
+import harryPotter.NotEnoughBooksInStockException;
+
+public class TestNotEnoughBooksInStock {
+
+    @Test
+    public void test() {
+        Book book1 = new Book("H1", 8, 1);
+        BookItem bookItem1;
+        try {
+            bookItem1 = new BookItem(book1, 2);
+        } catch (NotEnoughBooksInStockException e) {
+            // TODO Auto-generated catch block
+            return;
+        }
+        fail("Should not be here!");
+    }
+
+}
