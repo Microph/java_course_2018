e0e8dabd3c95b8b1509b6f85f28592659ebcab7a
Chanatip Kriengkraipetch <chanatip.kr@kbtg.tech>
2018-07-05 13:40:08 +0700

    Final form of functional program

--- src/main/java/day4/AbbreviationGen.java
+++ src/main/java/day4/AbbreviationGen.java
@@ -1,21 +1,18 @@
 package day4;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AbbreviationGen {
 
     public String generateAbbreviation(String string) {
-        String[] splittedStrings = string.split(" ");
-        String out = Arrays.stream(splittedStrings)
+        return Stream.of(string.split(" "))
             .map(String::toLowerCase)
             .filter(str -> str.compareTo("of") != 0)
             .filter(str -> str.compareTo("by") != 0)
             .map(str -> str.substring(0, 1))
             .map(String::toUpperCase)
             .collect(Collectors.joining( "" ));
-        
-        return out;
     }
 
 }
