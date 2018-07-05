f074951c512c57fa211e8d4c826fcba5db4393f8
Chanatip Kriengkraipetch <chanatip.kr@kbtg.tech>
2018-07-05 11:11:37 +0700

    Add stream collect demo

--- src/main/java/day4/MainStream.java
+++ src/main/java/day4/MainStream.java
@@ -3,6 +3,7 @@ package day4;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainStream {
@@ -15,14 +16,18 @@ public class MainStream {
         //Array Stream
         String[] data = new String[]{"One", "Two", "Three"};
         Stream<String> stream = Arrays.stream(data);
-        stream.forEach(System.out::print);
+        //stream.forEach(System.out::print);
         
         //Collection Stream
         List<String> list = new ArrayList<>();
         list.add("One");
         list.add("Two");
         list.add("Three");
-        Stream<String> stream2 = list.stream();
-        stream2.forEach(System.out::print);
+        //Stream<String> stream2 = list.stream();
+        //stream2.forEach(System.out::print);
+        
+        //Filter Demo
+        //Collectors.toList will convert Stream back to list to be reusable
+        list.stream().filter(string -> string.startsWith("T")).collect(Collectors.toList()).forEach(System.out::print);
     }
 }
