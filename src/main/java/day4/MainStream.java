aefe89afbd2e54de4151036cd33d3ed43d448b92
Chanatip Kriengkraipetch <chanatip.kr@kbtg.tech>
2018-07-05 10:49:14 +0700

    Add Java Stream basic demo

---
+++ src/main/java/day4/MainStream.java
@@ -0,0 +1,28 @@
+package day4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MainStream {
+    public static void main(String[] args) {
+        MainStream p = new MainStream();
+        p.start();
+    }
+
+    private void start() {
+        //Array Stream
+        String[] data = new String[]{"One", "Two", "Three"};
+        Stream<String> stream = Arrays.stream(data);
+        stream.forEach(System.out::print);
+        
+        //Collection Stream
+        List<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        Stream<String> stream2 = list.stream();
+        stream2.forEach(System.out::print);
+    }
+}
