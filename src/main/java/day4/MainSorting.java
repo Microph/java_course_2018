be91fc6862b81bacfac21911a1d360ec92422199
Chanatip Kriengkraipetch <chanatip.kr@kbtg.tech>
2018-07-05 10:22:54 +0700

    Java Day4 (Comparators)

---
+++ src/main/java/day4/MainSorting.java
@@ -0,0 +1,45 @@
+package day4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainSorting {
+    public static void main(String[] args) {
+        MainSorting p = new MainSorting();
+        p.start();
+    }
+
+    private void start() {
+        List<Employee> employees = new ArrayList<Employee>();
+        employees.add(new Employee(1, "Somkiat", 200));      
+        employees.add(new Employee(2, "Kiatsom", 300));
+        employees.add(new Employee(3, "SomSom", 500));
+        
+        //Anonymous class approach
+        /*Collections.sort(employees, new Comparator<Employee>() {
+
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                //-1 = Descending, 0 = Do nothing, 1 = Ascending
+                return e1.getName().compareTo(e2.getName());
+            }
+        });*/
+        
+        //2nd approach
+        Collections.sort(employees, new EmployeeSort());
+        
+        employees.forEach(System.out::println);
+    }
+    
+    //Using implemented class is more recommended
+    private class EmployeeSort implements Comparator<Employee>{
+        @Override
+        public int compare(Employee arg0, Employee arg1) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+    }
+        
+}
