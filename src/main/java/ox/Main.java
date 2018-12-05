5061b90f0b8130c433c6af6e25b3e42e8f468f9b
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2018-12-05 15:52:22 +0700

    Tables now print number index

--- src/main/java/ox/Main.java
+++ src/main/java/ox/Main.java
@@ -1,5 +1,6 @@
 package ox;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,30 +10,18 @@ public class Main {
     	int mode = reader.nextInt();
         OXGame game = new OXGame(3);
         game.setGameMode(mode);
-        startGame(game);
+        try {
+			startGame(game);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
         reader.close();
     }
 
-    private static void startGame(OXGame game) {
+    private static void startGame(OXGame game) throws IOException {
         //loop
         while(game.hasMovesLeft() && game.checkStateAtPoint(game.getLastMove().getRowIdx(), game.getLastMove().getColumnIdx()) == 0) {
-            //print table
-            for(int i=0; i<game.getTableSize(); i++) {
-                for(int j=0; j<game.getTableSize(); j++) {
-                    System.out.print("[");
-                    if(game.tableIndex(i, j) == "") {
-                        System.out.print(" ");
-                    }
-                    else {
-                        System.out.print(game.tableIndex(i, j));
-                    }
-                    System.out.print("]");
-                }
-                System.out.println();
-            }
-            System.out.println();
-            
-            //playing
+            printTable(game);
             if(game.getTurnOwner() == game.getPlayer1()) {
             	game.getPlayer1().play(game);
             }
@@ -42,22 +31,7 @@ public class Main {
             
         }
         
-        //end
-        for(int i=0; i<game.getTableSize(); i++) {
-            for(int j=0; j<game.getTableSize(); j++) {
-                System.out.print("[");
-                if(game.tableIndex(i, j) == "") {
-                    System.out.print(" ");
-                }
-                else {
-                    System.out.print(game.tableIndex(i, j));
-                }
-                System.out.print("]");
-            }
-            System.out.println();
-        }
-        
-        System.out.println();
+        printTable(game);
         if(game.determineBoardState() == 1)
         {
         	System.out.println(game.getPlayer2().getName() + " WINS!");
@@ -69,6 +43,29 @@ public class Main {
         else {
         	System.out.println("DRAW!");
         }
+        
+        System.in.read();
     }
+
+	private static void printTable(OXGame game) {
+		System.out.print("  0  1  2\n");
+		for(int i=0; i<game.getTableSize(); i++) {
+		    for(int j=0; j<game.getTableSize(); j++) {
+		    	if(j==0) {
+		    		System.out.print(i);
+		    	}
+		        System.out.print("[");
+		        if(game.tableIndex(i, j) == "") {
+		            System.out.print(" ");
+		        }
+		        else {
+		            System.out.print(game.tableIndex(i, j));
+		        }
+		        System.out.print("]");
+		    }
+		    System.out.println();
+		}
+		System.out.println();
+	}
     
 }
